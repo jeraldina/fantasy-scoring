dbcdca05168be1c2fa56b93070ca1b65d1797477
jeraldina <jeraldina@gmail.com>
2021-07-12 23:13:09 -0400

    adding qb stats

--- scoring.js
+++ scoring.js
@@ -2,17 +2,25 @@ const calculateScore = (player) => {
   switch (player.position) {
     case 'QB':
       return (
+        player.stats.passing.yards / 25 +
+                  player.stats.passing.touchdowns * 6 +
+                  player.stats.passing.interceptions * -3 +
+                  player.stats.rushing.yards / 10 +
+                  player.stats.rushing.touchdowns * 6 +
+                  player.stats.rushing.fumbles * -3
       )
     case 'RB':
       return (
+        
       )
     case 'WR':
       return (
-       
+        
       )
 
     case 'TE':
       return (
+        
       )
     case 'K':
       return (0)
