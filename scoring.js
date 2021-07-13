45d0c93ab0df91371c255da07b1df70d69ba1b38
jeraldina <jeraldina@gmail.com>
2021-07-12 23:02:02 -0400

    adding calculate score function, switch statement

---
+++ scoring.js
@@ -0,0 +1,22 @@
+const calculateScore = (player) => {
+  switch (player.position) {
+    case 'QB':
+      return (
+      )
+    case 'RB':
+      return (
+      )
+    case 'WR':
+      return (
+       
+      )
+
+    case 'TE':
+      return (
+      )
+    case 'K':
+      return (0)
+  }
+}
+
+module.exports = calculateScore
