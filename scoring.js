3edae004323d4a985fab024901e039f84395afc7
jeraldina <jeraldina@gmail.com>
2021-07-12 23:22:53 -0400

    adding TE stats

--- scoring.js
+++ scoring.js
@@ -27,12 +27,27 @@ const calculateScore = (player) => {
       )
     case 'WR':
       return (
-        
+        player.stats.rushing.yards / 10 +
+        player.stats.rushing.touchdowns * 6 +
+        player.stats.rushing.fumbles * -3 +
+        player.stats.receiving.receptions * 1 +
+        player.stats.receiving.yards / 10 +
+        player.stats.receiving.touchdowns * 6 +
+        player.stats.receiving.fumbles * -3 +
+        player.stats.return.kickreturn.yards / 15 +
+        player.stats.return.kickreturn.touchdowns * 6 +
+        player.stats.return.kickreturn.fumbles * -3 +
+        player.stats.return.puntreturn.yards / 15 +
+        player.stats.return.puntreturn.touchdowns * 6 +
+        player.stats.return.puntreturn.fumbles * -3
       )
 
     case 'TE':
       return (
-        
+        player.stats.receiving.receptions * 1 +
+        player.stats.receiving.yards / 10 +
+        player.stats.receiving.touchdowns * 6 +
+        player.stats.receiving.fumbles * -3
       )
     case 'K':
       return (0)
