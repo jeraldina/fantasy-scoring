c563f7beeb5f0fec536e3acd5d4af7632cf1bf91
jeraldina <jeraldina@gmail.com>
2021-07-12 23:20:46 -0400

    adding rb stats

--- scoring.js
+++ scoring.js
@@ -3,15 +3,27 @@ const calculateScore = (player) => {
     case 'QB':
       return (
         player.stats.passing.yards / 25 +
-                  player.stats.passing.touchdowns * 6 +
-                  player.stats.passing.interceptions * -3 +
-                  player.stats.rushing.yards / 10 +
-                  player.stats.rushing.touchdowns * 6 +
-                  player.stats.rushing.fumbles * -3
+        player.stats.passing.touchdowns * 6 +
+        player.stats.passing.interceptions * -3 +
+        player.stats.rushing.yards / 10 +
+        player.stats.rushing.touchdowns * 6 +
+        player.stats.rushing.fumbles * -3
       )
     case 'RB':
       return (
-        
+        player.stats.rushing.yards / 10 +
+        player.stats.rushing.touchdowns * 6 +
+        player.stats.rushing.fumbles * -3 +
+        player.stats.receiving.receptions * 1 +
+        player.stats.receiving.yards / 10 +
+        player.stats.receiving.touchdowns * 6 +
+        player.stats.receiving.fumbles * -3 +
+        player.stats.return.kickreturn.yards / 15 +
+        player.stats.return.kickreturn.touchdowns * 6 +
+        player.stats.return.kickreturn.fumbles * -3 +
+        player.stats.return.puntreturn.yards / 15 +
+        player.stats.return.puntreturn.touchdowns * 6 +
+        player.stats.return.puntreturn.fumbles * -3
       )
     case 'WR':
       return (
